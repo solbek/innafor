a1560a22ef7c94d98ab96bcab83399b913c15b84
eirikurdal <eirikurdal@gmail.com>
2018-10-24 11:42:02 +0200

    script i index.html er lagt i ekstern fil

--- public/script.js
+++ public/script.js
@@ -31,7 +31,8 @@ function loggInn() {
             output.innerHTML = json.mld;
             if (json.status == 200) {
                 console.log("yay");
-                window.location = json.forside;
+
+                //window.location = json.forside; // Denne linjen bytter html-fil.
             } else {
                 console.log("ops");
             }
@@ -40,7 +41,48 @@ function loggInn() {
             output.innerHTML = error;
             console.log(error);
         });
+}
 
 
+
+
+// DISPLAYKONTROLL -------------------------------------------------
+
+displayLogin();
+
+/*
+(function () {
+        if (! authenticatedUser) {
+            displayLoginForm();
+        } else{
+            displaySubmitRequestForm();
+        }
+    })();
+*/
+
+function addElement(element) {
+    document.getElementById("content").appendChild(element);
+}
+
+function clearScreen() {
+    document.getElementById("content").innerHTML = "";
+}
+
+function createElementFromTemplate(templateID) {
+    let template = document.querySelector(templateID);
+    let clone = document.importNode(template.content, true);
+    return clone;
+}
+
+function displayLogin() {
+    let login = createElementFromTemplate("#login")
+    addElement(login);
+
+    /// Sjekke om vi må legge inn login koder her.
+}
+
+function displayMainPage() {
+    let mainPage = createElementFromTemplate("#mainPage")
+    addElement(mainPage);
 
 }
