a251a7c5f3bfa5e3e2218c8211263bb9273c42a4
eirikurdal <eirikurdal@gmail.com>
2018-11-07 14:58:34 +0100

    igang med chart.html

--- public/js/chart_script.js
+++ public/js/chart_script.js
@@ -1,37 +1,232 @@
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
+let ctx = document.getElementById("myChart").getContext('2d');
+
+let answers = [
+    [4, 2, 5, 3, 4, 4, 1, 2, 3, 4],
+    [5, 3, 4, 4, 4, 4, 2, 3, 1, 3],
+    [1, 2, 1, 3, 2, 4, 1, 5, 1, 2],
+    [3, 2, 4, 2, 3, 4, 2, 1, 2, 3]
+];
+
+//Sorterte svar - Første array representerer første spørsmål.
+//Om array[0][0] = 3 betyr det at 3 besvarte spørsmål 1 med 1.
+let sortedAnswers = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]];
+
+/*let sortedAnswers = {
+    question1: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    },
+    question2: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    },
+    question3: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    },
+    question4: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    },
+    question5: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    },
+    question6: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    },
+    question7: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    },
+    question8: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    },
+    question9: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    },
+    question10: {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0
+    }
+};*/
+
+function sortAnswers() {
+    for (let i = 0; i < answers.length; i++) {
+        let player = answers[i];
+        for (let j = 0; j < player.length; j++) {
+            let answer = player[j];
+            if (answer == 1) {
+                sortedAnswers[j][1] += 1;
+            } else if (answer == 2) {
+                sortedAnswers[j][2] += 1;
+            } else if (answer == 3) {
+                sortedAnswers[j][3] += 1;
+            } else if (answer == 4) {
+                sortedAnswers[j][4] += 1;
+            } else if (answer == 5) {
+                sortedAnswers[j][5] += 1;
+            }
+        }
+    }
+}
+sortAnswers();
+console.log(sortedAnswers);
+
+let questions = ["Spm 1", "Spm 2", "Spm 3", "Spm 4", "Spm 5", "Spm 6", "Spm 7", "Spm 8", "Spm 9", "Spm 10"];
+let averageAnswers = [5, 3, 3, 1, 2, 3, 2, 4, 4, 5];
+let backgroundColors = [
+    'rgba(255, 206, 86, 0.8)',
+    'rgba(75, 192, 192, 0.8)',
+    'rgba(153, 102, 255, 0.8)',
+    'rgba(255, 206, 86, 0.8)',
+    'rgba(75, 192, 192, 0.8)',
+    'rgba(153, 102, 255, 0.8)',
+    'rgba(255, 206, 86, 0.8)',
+    'rgba(75, 192, 192, 0.8)',
+    'rgba(153, 102, 255, 0.8)',
+    'rgba(255, 206, 86, 0.8)',
+    'rgba(75, 192, 192, 0.8)',
+    'rgba(153, 102, 255, 0.8)',
+    'rgba(255, 206, 86, 0.8)',
+    'rgba(75, 192, 192, 0.8)',
+    'rgba(153, 102, 255, 0.8)',
+    'rgba(255, 206, 86, 0.8)',
+    'rgba(75, 192, 192, 0.8)',
+    'rgba(153,102, 255, 0.8)'];
+
+Chart.defaults.global.defaultFontColor = '#ffffff';
+Chart.defaults.global.legend.display = false;
+Chart.defaults.global.maintainAspectRatio = false;
+
+
+let myChart = new Chart(ctx, {
+    type: 'horizontalBar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ['1', '2', '3', '4', '5'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: 'Besvarelser',
+            data: averageAnswers,
+            backgroundColor: backgroundColors[0]
         }]
     },
     options: {
         scales: {
             yAxes: [{
                 ticks: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    stepSize: 1,
+                    fontSize: 16,
+                    fontStyle: 'bold'
+                },
+                gridLines: {
+                    display: false,
+                    color: 'rgba(255,255,255,1)'
+                },
+            }],
+            xAxes: [{
+                ticks: {
+                    min: 0
+                },
+                gridLines: {
+                    display: false,
+                    color: 'rgb(255,255,255)'
                 }
             }]
         }
     }
 });
+
+/*let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: questions,
+        datasets: [{
+            label: 'Gj.sitt',
+            data: averageAnswers,
+            backgroundColor: backgroundColors
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    max: 5,
+                    min: 0,
+                    stepSize: 1,
+                    fontSize: 16,
+                    fontStyle: 'bold'
+                },
+                gridLines: {
+                    display: false,
+                    color: 'rgba(255,255,255,1)'
+                },
+            }],
+            xAxes: [{
+                gridLines: {
+                    display: false,
+                    color: 'rgb(255,255,255)'
+                }
+            }]
+        }
+    }
+});*/
+
+
+// FUNCTIONS =========================
+
+function makeCharts() {
+    let div = document.createElement("div");
+    div.classList.add("horizontal-chart-container");
+    for (let i = 0; i < questions.length; i++) {
+        let html = "";
+        let questionTitle = questions[i];
+        html += `<h2>${questionTitle}</h2>`;
+        html += `<canvas id="canvas${i}"><cavnas>`;
+        let
+    }
+}
--- public/js/script.js
+++ public/js/script.js
@@ -1,85 +1,4 @@
-let status;
-
-        (function(){
-            getRefreshedStudentList();
-        })();
-
-
-
-function sendData(endpoint, data) {
-    return fetch(endpoint, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-        },
-        body: JSON.stringify(data)
-    }).then(data => {
-        status = data.status;
-        return data.json();
-    });
-}
-
-
-
-function getData(endpoint, data) {
-    return fetch(endpoint, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-        },
-        body: JSON.stringify(data)
-    }).then(data => {
-        console.log(data);
-    });
-}
-
-function getRefreshedStudentList(){
-    return fetch("/innafor/resultat/getUsers", {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-        },
-    }).then(data => {
-        console.log(data);
-    });
-        }
-
-
-//Sender inn logginn info for å teste på serveren
-function loggInn() {
-
-    let loggInnUsername = document.getElementById("logInnUsername");
-    let loggInnpassword = document.getElementById("loggInnpassword");
-    let output = document.getElementById("output");
-
-    let data = {
-        username: loggInnUsername.value,
-        password: loggInnpassword.value
-    };
-
-    sendData("/innafor/users/login", data)
-        .then(json => {
-            output.innerHTML = json.mld;
-            if (status == 200) {
-                console.log("yay");
-                displayMainPage();
-                //window.location = json.forside; // Denne linjen bytter html-fil.
-            } else {
-                console.log("ops");
-            }
-        })
-        .catch(error => {
-            output.innerHTML = error;
-            console.log(error);
-        });
-}
-
-
-
-
 // DISPLAYKONTROLL -------------------------------------------------
-
-
 
 // LOGIN USIKKER
 let loggedIn = true;
@@ -148,11 +67,6 @@ function displayProfile() {
     clearScreen();
     let jervProfile = createElementFromTemplate("#profile")
     addElement(jervProfile);
-}
-function displayResults() {
-    clearScreen();
-    let results = createElementFromTemplate("#results")
-    addElement(results);
 }
 
 // DISPLAYKONTROLL IFRAME -----------------------------
