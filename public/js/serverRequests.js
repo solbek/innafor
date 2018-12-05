0da27a4f60f820a7f4c22d920ae92946cc251b16
eirikurdal <eirikurdal@gmail.com>
2018-12-05 14:17:23 +0100

    local fontawesome og bootstrap

--- public/js/serverRequests.js
+++ public/js/serverRequests.js
@@ -1,11 +1,11 @@
-let url = 'https://innafor.herokuapp.com';
+let url = 'http://innafor.herokuapp.com';
 
 // (url+endpoint)
 
 
 // SEND DATA TO SERVER ============================
 function sendData(endpoint, data) {
-    return fetch(endpoint, {
+    return fetch((url+endpoint), {
         method: "POST",
         headers: {
             "Content-Type": "application/json; charset=utf-8",
@@ -18,7 +18,7 @@ function sendData(endpoint, data) {
 
 // GET DATA FROM SERVER ============================
 function getData(endpoint) {
-    return fetch(endpoint, {
+    return fetch((url+endpoint), {
         method: "GET",
         headers: {
             "Content-Type": "application/json; charset=utf-8",
