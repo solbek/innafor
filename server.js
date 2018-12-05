e2be554819d39fbac0b3f322e231dcd743cb929e
VValley <vedalhenrik@gmail.com>
2018-12-05 18:13:40 +0100

    allow header

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
     res.set("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-    res.set('Access-Control-Allow-Headers', '*');
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
     next(); //go to the specified route
 });
 
